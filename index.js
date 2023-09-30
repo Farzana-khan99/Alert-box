0ac04e783b7d7e2ef56586af458ec0c733fc4b4e
Farzana <id.farzankhan@gmail.com>
2023-09-30 22:39:25 +0500

    assigment 01 alert box

---
+++ index.js
@@ -0,0 +1,37 @@
+
+// 1. Write a script to greet your website visitor using JS alert
+// box.
+
+let greet = "Hi! whatapp "
+alert(greet)
+// 2. Write a script to display following message on your web
+// page:
+
+let errorPassword ="Error! please  inter valid password"
+
+alert(errorPassword)
+
+// 3. Write a script to display following message on your web
+// page: (Hint : Use line break)
+
+let messages = "Welcome to js Land... "
+
+alert(messages+ "\n" +" Happy coding")
+
+// 4. Write a script to display following messages in sequence:
+
+let message = "Welcome to js Land... "
+alert(message)
+
+let messageBox = "Happy coding!"
+alert(messageBox + "\n" + "Prevent this page from creating additional dialogs") 
+
+// 6. Make use of alerts in your new/existing HTML & CSS
+// project.
+// 7. Practice placement of <script></script> element in
+// following sections of your project in exercise 6:
+// a. Head
+// b. Body (before your page’s HTML)
+// c. Body (inside your page’s HTML)
+
+//  ===============THE END========================
